238edb1e2df3b4205ec86b7de56e37f36d4e9238
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-05-16 22:08:51 -0400

    created grid and attemptd colour change

--- Sketch.java
+++ Sketch.java
@@ -1,31 +1,52 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+  float CELL_WIDTH = 45;
+  float CELL_HEIGHT = 45;
+  float MARGIN = 5;
+  int ROW_COUNT = 10;
+  int COLUMN_COUNT = 10;
+  float SCREEN_WIDTH = (10*CELL_WIDTH * 11*MARGIN);
+  float SCREEN_HEIGHT = (10*CELL_HEIGHT * 11*MARGIN);
+  float rectX = 0;
+  float rectY = 0;
+  int[][] intGrid;
+  int row = 0;
+  int column = 0;
+
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+ 
   public void settings() {
-    // put your size call here
-    size(500, 600);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  
   public void setup() {
-    background(255,255,255);
+    background(0,0,0);
+    
+    intGrid = new int[ROW_COUNT][COLUMN_COUNT];
+
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+ 
   public void draw() {
-	  
-  }
+    for(row = 0; row < ROW_COUNT; row++){
+      for(column = 0; column < COLUMN_COUNT; column++){
+        rect(MARGIN,rectY,CELL_WIDTH,CELL_HEIGHT);
+        MARGIN+=49;
+      }
+      MARGIN = 5;
+      rectY +=50;
+    }
 
-  public void mousePressed(){
     
   }
-}
+
+  
+
+  public void mousePressed(){
+      fill(0,255,0);
+      rect(12,mouseY,CELL_WIDTH,CELL_HEIGHT);
+    }
+  }
