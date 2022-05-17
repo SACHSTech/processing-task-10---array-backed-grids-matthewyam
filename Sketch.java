743f852d5251505a59da827e2e5e78197a92b5af
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-05-16 23:00:45 -0400

    attempted mouse click

--- Sketch.java
+++ Sketch.java
@@ -2,21 +2,17 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-  float CELL_WIDTH = 45;
-  float CELL_HEIGHT = 45;
-  float MARGIN = 5;
+  int CELL_WIDTH = 45;
+  int CELL_HEIGHT = 45;
+  int MARGIN = 5;
   int ROW_COUNT = 10;
   int COLUMN_COUNT = 10;
   float SCREEN_WIDTH = (10*CELL_WIDTH * 11*MARGIN);
   float SCREEN_HEIGHT = (10*CELL_HEIGHT * 11*MARGIN);
-  float rectX = 0;
-  float rectY = 0;
+  int rectX = 0;
+  int rectY = 0;
   int[][] intGrid;
-  int row = 0;
-  int column = 0;
 
-	
- 
   public void settings() {
     size(500, 500);
   }
@@ -26,13 +22,25 @@ public class Sketch extends PApplet {
     background(0,0,0);
     
     intGrid = new int[ROW_COUNT][COLUMN_COUNT];
+    for(int x = 0; x < ROW_COUNT; x++){
+      for(int y = 0; y < COLUMN_COUNT; y++){
+        intGrid[y][x] = 0;
+      }
+    }
 
   }
+  
 
  
   public void draw() {
-    for(row = 0; row < ROW_COUNT; row++){
-      for(column = 0; column < COLUMN_COUNT; column++){
+    for(int row = 0; row < ROW_COUNT; row++){
+      for(int column = 0; column < COLUMN_COUNT; column++){
+        if(intGrid[column][row] == 1){
+          fill(0,255,0);
+        }
+        else{
+          fill(255);
+        }
         rect(MARGIN,rectY,CELL_WIDTH,CELL_HEIGHT);
         MARGIN+=49;
       }
@@ -42,11 +50,17 @@ public class Sketch extends PApplet {
 
     
   }
+  public void mousePressed(){
+    if(intGrid[mouseY/CELL_WIDTH][mouseX/CELL_HEIGHT] == 1){
+      intGrid[mouseY/CELL_WIDTH][mouseX/CELL_HEIGHT] = 0;
+
+    }
+    else{
+      intGrid[mouseY/CELL_WIDTH][mouseX/CELL_HEIGHT] = 1;
+    }
+  }
+}
 
   
 
-  public void mousePressed(){
-      fill(0,255,0);
-      rect(12,mouseY,CELL_WIDTH,CELL_HEIGHT);
-    }
-  }
+  
