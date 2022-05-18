4a0d57a3605e9cdc7d81b340cc40406c229514de
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-05-17 21:34:50 -0400

    done colour click

--- Sketch.java
+++ Sketch.java
@@ -5,15 +5,18 @@ public class Sketch extends PApplet {
   int CELL_WIDTH = 45;
   int CELL_HEIGHT = 45;
   int MARGIN = 5;
+  
   int ROW_COUNT = 10;
   int COLUMN_COUNT = 10;
+  
   int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
   int SCREEN_HEIGHT = (CELL_HEIGHT + MARGIN) * ROW_COUNT + MARGIN;
   int[][] intGrid;
+  
   int row = 0;
   int column = 0;
   
-
+ 
   
   public void settings() {
     size(500, 500);
@@ -35,40 +38,39 @@ public class Sketch extends PApplet {
 
  
   public void draw() {
+    intGrid[9][2] = 1;
     for(row = 0; row < ROW_COUNT; row++){
       for(column = 0; column < COLUMN_COUNT; column++){
+        int boxX = (MARGIN + CELL_WIDTH) * column + MARGIN + CELL_HEIGHT/2 -25;
+        int boxY = (MARGIN + CELL_HEIGHT) * row + MARGIN + CELL_HEIGHT/2 -25;
         if(intGrid[column][row] == 1){
           fill(0,255,0);
-        }
-        else{
-          fill(255);
-        }
-          int boxX = (MARGIN + CELL_WIDTH) * column + MARGIN + CELL_HEIGHT/2 -25;
-          int boxY = (MARGIN + CELL_HEIGHT) * row + MARGIN + CELL_HEIGHT/2 -25;
-
           rect(boxX,boxY,CELL_WIDTH,CELL_HEIGHT);
+        }
+        if(intGrid[column][row] == 0){
+          fill(255);
+          rect(boxX,boxY,CELL_WIDTH,CELL_HEIGHT);
+        }
+      }  
 
       }
 
     }
     
-  }
+  
   public void mousePressed(){
     
     
-    //column = (boxX / (CELL_WIDTH + MARGIN));
-    //row = (boxY / (CELL_HEIGHT + MARGIN));
-    
-    if(intGrid[row][column] == 1){
-      intGrid[row][column] = 0;
-
+    if(intGrid [mouseX/50][mouseY/50] == 1){
+      intGrid [mouseX/50][mouseY/50] = 0;
     }
+  
     else{
-      intGrid[row][column] = 1;
+      intGrid [mouseX/50][mouseY/50] = 1;
     }
   }
-  
 }
+
 
   
 
