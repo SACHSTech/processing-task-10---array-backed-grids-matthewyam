2a29ba2132206c9a9ad5f1dcf4620280c75b901e
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-05-19 21:26:41 -0400

    done

--- Sketch.java
+++ Sketch.java
@@ -9,8 +9,8 @@ public class Sketch extends PApplet {
   int ROW_COUNT = 10;
   int COLUMN_COUNT = 10;
   
-  int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
-  int SCREEN_HEIGHT = (CELL_HEIGHT + MARGIN) * ROW_COUNT + MARGIN;
+  int SCREEN_WIDTH = 500;
+  int SCREEN_HEIGHT = 500;
   int[][] intGrid;
   
   int row = 0;
@@ -21,11 +21,13 @@ public class Sketch extends PApplet {
   int rowCellCount = 0;
   int colCellCount = 0;
 
+  int rowCont = 0;
+
   
  
   
   public void settings() {
-    size(500, 500);
+    size(SCREEN_WIDTH, SCREEN_HEIGHT);
   }
 
   
@@ -41,14 +43,15 @@ public class Sketch extends PApplet {
 
   }
   
-
  
   public void draw() {
+    //Draws Grid
     for(row = 0; row < ROW_COUNT; row++){
       for(column = 0; column < COLUMN_COUNT; column++){
         int boxX = (MARGIN + CELL_WIDTH) * column + MARGIN + CELL_HEIGHT/2 -25;
         int boxY = (MARGIN + CELL_HEIGHT) * row + MARGIN + CELL_HEIGHT/2 -25;
 
+        //Sets Colours
         if(intGrid[column][row] == 1){
           fill(0,255,0);
           rect(boxX,boxY,CELL_WIDTH,CELL_HEIGHT);
@@ -63,11 +66,15 @@ public class Sketch extends PApplet {
 
     }
     
+    /**
+     * Checks if the mouse is pressed
+     */
   
   public void mousePressed(){
     int colCheck = mouseX/50;
     int rowCheck = mouseY/50;
     
+    //Checks how many cells are selected
     if(intGrid [mouseX/50][mouseY/50] == 1){
 
       intGrid [mouseX/50][mouseY/50] = 0;
@@ -83,8 +90,7 @@ public class Sketch extends PApplet {
       
     }
   }
-//
-
+//Makes Cells go in cross shape
       if(colCheck < 9 && intGrid [mouseX/50+1][mouseY/50] == 1 ){
         
         intGrid [mouseX/50+1][mouseY/50] = 0;
@@ -101,7 +107,6 @@ public class Sketch extends PApplet {
         
     }
     }
-//
     if(colCheck > 0 && intGrid [mouseX/50-1][mouseY/50] == 1){ 
 
         intGrid [mouseX/50-1][mouseY/50] = 0;
@@ -115,7 +120,6 @@ public class Sketch extends PApplet {
         
       }
     }
-//
     if(rowCheck < 9 && intGrid [mouseX/50][mouseY/50+1] == 1){
         intGrid [mouseX/50][mouseY/50+1] = 0;
         cellCounter -=1;
@@ -130,7 +134,6 @@ public class Sketch extends PApplet {
        
       }
     }
-//
     if(rowCheck > 0 && intGrid [mouseX/50][mouseY/50-1] == 1){
         intGrid [mouseX/50][mouseY/50-1] = 0;
         cellCounter -=1;
@@ -144,33 +147,64 @@ public class Sketch extends PApplet {
         
       }
     }
+    //Prints mouse clicks and total amount of cells are selected
+    System.out.println("");
     System.out.println("Click " + "Mouse Coordinates: " + mouseX + " " + mouseY + " Grid Coordinates: (Row: " + mouseY/50 + " Column: " + mouseX/50 + ")");
-        System.out.println("Total of " + cellCounter + " cells are selected");
-        System.out.println("");
+    System.out.println("Total of " + cellCounter + " cells are selected");
+    System.out.println("");
     
 
-        for(int i = 0; i < ROW_COUNT; i++){
-          for (int j = 0; j < COLUMN_COUNT; j++){
-          if(intGrid[j][i] == 1){
-            rowCellCount++;
+    for(int i = 0; i < ROW_COUNT; i++){
+      for (int j = 0; j < COLUMN_COUNT; j++){
+      
+        //Checks how many cells are selected per row
+        if(intGrid[j][i] == 1){
+          rowCellCount++;
+        }
+        //Checks how many cells are selected consecutively
+        if(i > 0 && i < 9){
+          if(intGrid[j][i] == 1 && intGrid[j][i+1] == 1){
+            rowCont+=1;
           }
-        }
-        System.out.println("There are " + rowCellCount + " cells selected on Row " + i);
-        rowCellCount = 0;
+            
+          if(intGrid[j][i] == 1 && intGrid[j][i-1] == 1){
+            rowCont+=1;
+          }
+          
+          if(intGrid[j][i] == 1 && intGrid[j][i-1] == 1 && intGrid[j][i+1] == 1 && i < 9){
+            rowCont+=1;
+            
+           }
+            
+          }
         
-        
+
         }
-        System.out.println("");
-        for (int l = 0; l < COLUMN_COUNT; l++){
-          for(int k = 0; k < ROW_COUNT; k++){
+        //Prints how manny cells are selected per row and consecutively
+        if (rowCont > 0 && rowCont < 9  && rowCellCount > 2){ 
+          System.out.println("There are " + rowCont + " consecutive cells selected on Row " + i);
+          rowCont = 0;
+          }
+          System.out.println("There are " + rowCellCount + " cells selected on Row " + i);
+          rowCellCount = 0;
+
+        }
+
+      
+      System.out.println("");
+      
+       //Checks how many cells are selected per column
+      for (int l = 0; l < COLUMN_COUNT; l++){
+        for(int k = 0; k < ROW_COUNT; k++){
           if(intGrid[l][k] == 1){
-            colCellCount++;
-          }
-        }
-        System.out.println("There are " + colCellCount + " cells selected on Column " + l);
+           colCellCount++;
+         }
+       }
+       System.out.println("There are " + colCellCount + " cells selected on Column " + l);
         colCellCount = 0;
 
       }
+      
     }
   
     
